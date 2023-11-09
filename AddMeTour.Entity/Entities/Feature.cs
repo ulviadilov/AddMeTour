cbdc4bba8ee20cc198dde531c01f02bf41f92402
ulviadilov <112329823+ulviadilov@users.noreply.github.com>
2023-11-09 06:45:07 +0400

    Project and Layers created

---
+++ AddMeTour.Entity/Entities/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddMeTour.Entity.Entities
+{
+    public class Feature
+    {
+        public Guid InformationId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

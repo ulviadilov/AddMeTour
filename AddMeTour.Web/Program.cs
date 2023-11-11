688f6f1ac819f8086a81a6d3698967ac38aa8694
ulviadilov <112329823+ulviadilov@users.noreply.github.com>
2023-11-12 00:50:56 +0400

    Area added and Routing created

--- AddMeTour.Web/Program.cs
+++ AddMeTour.Web/Program.cs
@@ -24,10 +24,15 @@ namespace AddMeTour
             app.UseRouting();
 
             app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                    name: "Admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+                    );
+                endpoints.MapDefaultControllerRoute();
+            });
 
             app.Run();
         }

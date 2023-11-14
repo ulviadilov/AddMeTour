896545bcedbce81f3dad69e9c28afd1397328157
ulviadilov <112329823+ulviadilov@users.noreply.github.com>
2023-11-14 16:06:39 +0400

    Front to back

--- AddMeTour.Web/Program.cs
+++ AddMeTour.Web/Program.cs
@@ -13,7 +13,7 @@ namespace AddMeTour
             // Add services to the container.
             builder.Services.LoadDataLayerExtension(builder.Configuration);
             builder.Services.LoadServiceLayerExtension();
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             var app = builder.Build();
 

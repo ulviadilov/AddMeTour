0640f15544f5efceb743ec0211444b90cfa9cb9c
AliIbrahimov <aliibrah30@gmail.com>
2024-03-12 20:19:30 +0400

    add review to UI

--- AddMeTour.Data/obj/Debug/net7.0/AddMeTour.Data.AssemblyInfo.cs
+++ AddMeTour.Data/obj/Debug/net7.0/AddMeTour.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddMeTour.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9baae0ea218551808e8c9576be339f9eb5211ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddMeTour.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddMeTour.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
